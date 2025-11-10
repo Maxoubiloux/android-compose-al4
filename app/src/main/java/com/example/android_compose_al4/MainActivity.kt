2f87b27b97248cc31639648a4e2660127b04b696
Maxoubiloux <max.kovacevic@laposte.net>
2025-11-10 22:15:08 +0100

    feat: initial architecture

--- app/src/main/java/com/example/android_compose_al4/MainActivity.kt
+++ app/src/main/java/com/example/android_compose_al4/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_compose_al4.nav.AppNavHost
 import com.example.android_compose_al4.ui.theme.Androidcomposeal4Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Androidcomposeal4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppNavHost()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     Androidcomposeal4Theme {
-        Greeting("Android")
+        AppNavHost()
     }
 }
---
+++ app/src/main/java/com/example/android_compose_al4/nav/NavRoutes.kt
@@ -0,0 +1,8 @@
+package com.example.android_compose_al4.nav
+
+object NavRoutes {
+    const val LOGIN = "login"
+    const val PIN = "pin"
+    const val HOME = "home"
+}
+
